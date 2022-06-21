89e887bba95f948e22f0ac8be7dcdca13af0cecb
ilham bouaik <ibouaik@gmail.com>
2022-06-21 10:19:45 +0100

    add css to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,35 +4,35 @@ import planet from "../images/planet.png";
 import "./NavBar.css";
 
 const Navbar = () => {
-  const navLinkStyles = ({ isActive }) => {
-    return {
-      fontWeight: isActive ? "bold" : "normal",
-      textDecoration: isActive ? "none" : "underline",
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            fontWeight: isActive ? "bold" : "normal",
+            textDecoration: isActive ? "none" : "underline",
+        };
     };
-  };
 
-  return (
-    <div className="navBar-container d-flex">
-      <div className="logo-container d-flex">
-        <img src={planet} />
-        <h2>Space Travelers' Hub</h2>
-      </div>
-      <nav>
-        <NavLink style={navLinkStyles} to="/">
-          {" "}
-          Rockets
-        </NavLink>
-        <NavLink style={navLinkStyles} to="/Missions">
-          {" "}
-          Missions
-        </NavLink>
-        <NavLink style={navLinkStyles} to="/Profile">
-          {" "}
-          Profile
-        </NavLink>
-      </nav>
-    </div>
-  );
+    return (
+        <div className="navBar-container d-flex">
+            <div className="logo-container d-flex">
+                <img src={planet} />
+                <h2>Space Travelers' Hub</h2>
+            </div>
+            <nav>
+                <NavLink style={navLinkStyles} to="/">
+                    {" "}
+                    Rockets
+                </NavLink>
+                <NavLink style={navLinkStyles} to="/Missions">
+                    {" "}
+                    Missions
+                </NavLink>
+                <NavLink className="profile" style={navLinkStyles} to="/Profile">
+                    {" "}
+                    Profile
+                </NavLink>
+            </nav>
+        </div>
+    );
 };
 
 export default Navbar;
