aecf23565502f5e07c53d8c293706dbc368774b1
ilham bouaik <ibouaik@gmail.com>
2022-06-21 12:42:19 +0100

    start with rockets Id

---
+++ src/Redux/Rockets/RocketsSlice.js
@@ -0,0 +1,12 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+const url = "// https://api.spacexdata.com/v3/rockets"
+
+import React from 'react'
+
+const RocketsSlice = () => {
+    return (
+        <div>RocketsSlice</div>
+    )
+}
+
+export default RocketsSlice
--- src/components/pages/Rockets.js
+++ src/components/pages/Rockets.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./Rockets.css"
-import image from "../pages/download.jpeg"
 function Rockets() {
   return <div>
     <header className="container">
       <div className="image" >
-        <img src={image}></img>
+        <img src=""></img>
       </div>
       <div className="info">
         <h1>Falcon1</h1>
@@ -14,52 +13,6 @@ function Rockets() {
         <button className="btn">Reserve Rocket</button>
       </div>
     </header>
-    <header className="container">
-      <div className="image" >
-        <img src={image}></img>
-      </div>
-      <div className="info">
-        <h1>Falcon1</h1>
-        <p className="pargraph">Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or another type of compatible computer.
-        </p>
-        <button className="btn">Reserve Rocket</button>
-      </div>
-    </header>
-    <header className="container">
-      <div className="image" >
-        <img src={image}></img>
-      </div>
-      <div className="info">
-        <h1>Falcon1</h1>
-        <p className="pargraph">Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or another type of compatible computer.
-        </p>
-        <button className="btn">Reserve Rocket</button>
-      </div>
-    </header>
-    <header className="container">
-      <div className="image" >
-        <img src={image}></img>
-      </div>
-      <div className="info">
-        <h1>Falcon1</h1>
-        <p className="pargraph">Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or another type of compatible computer.
-        </p>
-        <button className="btn">Reserve Rocket</button>
-      </div>
-    </header>
-    <header className="container">
-      <div className="image" >
-        <img src={image}></img>
-      </div>
-      <div className="info">
-        <h1>Falcon1</h1>
-        <p className="pargraph">Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or another type of compatible computer.
-        </p>
-        <button className="btn">Reserve Rocket</button>
-      </div>
-    </header>
-
-
 
   </div>;
 }
